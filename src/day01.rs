c8f31e1618d67651c1320697aef44e530e288ae3
Erasmus Cedernaes <github@emaus.dev>
2025-12-01 21:21:14 +0100

    Day 1 part 2

--- src/day01.rs
+++ src/day01.rs
@@ -25,9 +25,35 @@ pub fn part1(input: &str) -> i32 {
     zeroes
 }
 
-pub fn part2(input: &str) -> String {
-    // TODO: Implement part 2
-    todo!("Part 2 not yet implemented")
+pub fn part2(input: &str) -> i32 {
+    let mut offset = 50;
+    let mut zeroes = 0;
+
+    for line in input.lines() {
+        let dir = &line[0..1];
+        let mut value: i32 = match line[1..].parse() {
+            Ok(v) => v,
+            Err(_) => panic!("Failed to parse integer from line: {}", &line[1..]),
+        };
+
+        while value > 0 {
+            if dir == "L" {
+                offset -= 1;
+                value -= 1;
+            } else {
+                offset += 1;
+                value -= 1;
+            }
+
+            offset = offset % 100;
+
+            if offset == 0 {
+                zeroes += 1;
+            }
+        }
+    }
+
+    zeroes
 }
 
 #[cfg(test)]
@@ -41,11 +67,9 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part2_example() {
         let input = std::fs::read_to_string("day01/example2.txt").expect("Example file not found");
-        todo!();
-        assert_eq!(3, part1(&input));
+        assert_eq!(6, part2(&input));
     }
 
     #[test]
@@ -55,7 +79,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part2_input() {
         let input = std::fs::read_to_string("day01/input.txt").expect("Input file not found");
         println!("Part 2: {}", part2(&input));
