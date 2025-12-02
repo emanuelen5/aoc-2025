f18718c333ebcb241c52fe85d2458051e21572a4
Erasmus Cedernaes <github@emaus.dev>
2025-12-02 22:03:03 +0100

    Day 2 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -19,7 +19,7 @@ pub fn part1(input: &str) -> i64 {
             }
 
             let (left, right) = current_str.split_at(length / 2);
-            if (left == right) {
+            if left == right {
                 matching_numbers.push(current);
             }
 
@@ -31,7 +31,51 @@ pub fn part1(input: &str) -> i64 {
 }
 
 pub fn part2(input: &str) -> i64 {
-    todo!("Part 2 not yet implemented")
+    let mut matching_numbers = Vec::<i64>::new();
+
+    // Input format: "11-22,33-44,55-66,..."
+    for substr in input.trim().split(",") {
+        let range_parts: Vec<&str> = substr.split('-').collect();
+        let start: i64 = range_parts[0].trim().parse().unwrap();
+        let end: i64 = range_parts[1].trim().parse().unwrap();
+
+        // println!("Processing range {}-{}", start, end);
+
+        let mut current = start;
+        while current <= end {
+            let current_str = current.to_string();
+            let length = current_str.len();
+
+            // println!("Checking number {}", current);
+
+            for group_length in 1..=length / 2 {
+                if length % group_length != 0 && group_length != 1 {
+                    continue;
+                }
+
+                // println!("Checking number {} with group length {}", current, group_length);
+
+                let mut all_match = true;
+                let first_group = &current_str[0..group_length];
+                for i in (group_length..length).step_by(group_length) {
+                    let next_group = &current_str[i..i + group_length];
+                    if first_group != next_group {
+                        all_match = false;
+                        break;
+                    }
+                }
+
+                if all_match {
+                    matching_numbers.push(current);
+                    break;
+                }
+            }
+
+            current += 1;
+        }
+    }
+
+    matching_numbers.iter().sum()
 }
 
 #[cfg(test)]
@@ -45,11 +89,9 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part2_example() {
-        let input = std::fs::read_to_string("day02/example2.txt").expect("Example file not found");
-        todo!();
-        assert_eq!(3, part1(&input));
+        let input = std::fs::read_to_string("day02/example1.txt").expect("Example file not found");
+        assert_eq!(4174379265, part2(&input));
     }
 
     #[test]
@@ -59,7 +101,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part2_input() {
         let input = fs::read_to_string("day02/input.txt").expect("Input file not found");
         println!("Part 2: {}", part2(&input));
