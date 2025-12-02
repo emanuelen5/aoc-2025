ac8a58c4ec2062028caf1af6d21a4ebc661ebf65
Erasmus Cedernaes <github@emaus.dev>
2025-12-02 22:04:11 +0100

    Update template

--- template.rs
+++ template.rs
@@ -1,10 +1,10 @@
 use std::fs;
 
-pub fn part1(input: &str) -> String {
+pub fn part1(input: &str) -> i32 {
     todo!("Part 1 not yet implemented")
 }
 
-pub fn part2(input: &str) -> String {
+pub fn part2(input: &str) -> i32 {
     todo!("Part 2 not yet implemented")
 }
 
@@ -21,9 +21,8 @@ mod tests {
     #[test]
     #[ignore]
     fn test_part2_example() {
-        let input = std::fs::read_to_string("dayXX/example2.txt").expect("Example file not found");
-        todo!();
-        assert_eq!(3, part1(&input));
+        let input = std::fs::read_to_string("dayXX/example1.txt").expect("Example file not found");
+        assert_eq!(3, part2(&input));
     }
 
     #[test]
