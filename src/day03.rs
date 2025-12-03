5d85cfde68f357c802d2e8b94d5abe492707e3e5
Erasmus Cedernaes <github@emaus.dev>
2025-12-03 21:23:39 +0100

    Day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -3,51 +3,30 @@ struct Digit {
     offset: i32,
 }
 
-fn joltage(line: &str) -> i32 {
-    // All but the last character
-    let mut max_digit0: Digit = Digit {
-        value: 0,
-        offset: -1,
-    };
-    for (offset, c) in line.chars().take(line.len() - 1).enumerate() {
-        let digit: u8 = match c.to_digit(10) {
-            Some(d) => d as u8,
-            None => panic!("Invalid character {c} in input"),
-        };
-
-        match digit {
-            // We can't find a better value
-            9 => {
-                max_digit0 = Digit {
-                    value: 9,
-                    offset: offset as i32,
-                };
-                break;
-            }
-            _ => {
-                if digit > max_digit0.value {
-                    max_digit0 = Digit {
-                        value: digit,
-                        offset: offset as i32,
-                    };
-                }
-            }
-        }
+fn joltage(line: &str, digits: u8) -> i64 {
+    let mut total: i64 = 0;
+    let mut start_offset: usize = 0;
+    for digit in 0..digits {
+        total *= 10;
+        let max_digit_i: Digit = max_digit(line, start_offset, digits - digit);
+        total += max_digit_i.value as i64;
+        start_offset = max_digit_i.offset as usize + 1;
     }
 
-    if max_digit0.offset == -1 {
-        panic!("No digits found in line");
-    }
+    total
+}
 
+fn max_digit(line: &str, start_offset: usize, digits: u8) -> Digit {
     // Go through the rest to find the best second digit
-    let mut max_digit1: Digit = Digit {
+    let mut max_digit: Digit = Digit {
         value: 0,
         offset: -1,
     };
     for (offset, c) in line
         .chars()
         .enumerate()
-        .skip((max_digit0.offset + 1) as usize)
+        .take(line.len() - (digits as usize) + 1)
+        .skip(start_offset as usize)
     {
         let digit: u8 = match c.to_digit(10) {
             Some(d) => d as u8,
@@ -57,15 +36,15 @@ fn joltage(line: &str) -> i32 {
         match digit {
             // We can't find a better value
             9 => {
-                max_digit1 = Digit {
+                max_digit = Digit {
                     value: 9,
                     offset: offset as i32,
                 };
                 break;
             }
             _ => {
-                if digit > max_digit1.value {
-                    max_digit1 = Digit {
+                if digit > max_digit.value {
+                    max_digit = Digit {
                         value: digit,
                         offset: offset as i32,
                     };
@@ -74,23 +53,27 @@ fn joltage(line: &str) -> i32 {
         }
     }
 
-    if max_digit1.offset == -1 {
-        panic!("No second digit found in line");
+    if max_digit.offset == -1 {
+        panic!("No valid digits found in line after offset {start_offset}");
     }
 
-    (max_digit0.value as i32) * 10 + (max_digit1.value as i32)
+    max_digit
 }
 
-pub fn part1(input: &str) -> i32 {
-    let mut total = 0;
+pub fn part1(input: &str) -> i64 {
+    let mut total: i64 = 0;
     for line in input.lines() {
-        total += joltage(line);
+        total += joltage(line, 2);
     }
     total
 }
 
-pub fn part2(input: &str) -> i32 {
-    todo!("Part 2 not yet implemented")
+pub fn part2(input: &str) -> i64 {
+    let mut total: i64 = 0;
+    for line in input.lines() {
+        total += joltage(line, 12);
+    }
+    total
 }
 
 #[cfg(test)]
@@ -104,10 +87,9 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part2_example() {
         let input = std::fs::read_to_string("day03/example1.txt").expect("Example file not found");
-        assert_eq!(3, part2(&input));
+        assert_eq!(3121910778619, part2(&input));
     }
 
     #[test]
@@ -117,7 +99,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part2_input() {
         let input = std::fs::read_to_string("day03/input.txt").expect("Input file not found");
         println!("Part 2: {}", part2(&input));
