12d1442cc4027fb42c28fd3e328809fee93cc686
Erasmus Cedernaes <github@emaus.dev>
2025-12-06 08:53:59 +0100

    Day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -42,8 +42,8 @@ fn neighbors(arr: &Vec<Vec<bool>>, row: usize, col: usize) -> u32 {
     count
 }
 
-pub fn part1(input: &str) -> i32 {
-    let arr = parse_array(input);
+fn remove_accessible_rolls(arr: &mut Vec<Vec<bool>>) -> u32 {
+    let mut to_remove: Vec<(usize, usize)> = Vec::new();
     let mut total_with_less_than_4_neighbors = 0;
     for i in 1..(arr.len() - 1) {
         for j in 1..(arr[0].len() - 1) {
@@ -54,14 +54,32 @@ pub fn part1(input: &str) -> i32 {
             let n = neighbors(&arr, i, j);
             if n < 4 {
                 total_with_less_than_4_neighbors += 1;
+                to_remove.push((i, j));
             }
         }
+    }
+    for (i, j) in to_remove {
+        arr[i][j] = false;
     }
     total_with_less_than_4_neighbors
 }
 
-pub fn part2(input: &str) -> i32 {
-    todo!("Part 2 not yet implemented")
+pub fn part1(input: &str) -> u32 {
+    let mut arr = parse_array(input);
+    remove_accessible_rolls(&mut arr)
+}
+
+pub fn part2(input: &str) -> u32 {
+    let mut arr = parse_array(input);
+    let mut removed = 0;
+    loop {
+        let n = remove_accessible_rolls(&mut arr);
+        if n == 0 {
+            break;
+        }
+        removed += n;
+    }
+    removed
 }
 
 #[cfg(test)]
@@ -81,14 +99,12 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part2_example() {
         let input = std::fs::read_to_string("day04/example1.txt").expect("Example file not found");
-        assert_eq!(3, part2(&input));
+        assert_eq!(43, part2(&input));
     }
 
     #[test]
-    #[ignore]
     fn test_part2_input() {
         let input = std::fs::read_to_string("day04/input.txt").expect("Input file not found");
         println!("Part 2: {}", part2(&input));
